af1552936ec84da9c4c7cc58fb9b5500349a19ee
kawsarm104 <kawsarm104@gmail.com>
2021-02-04 23:05:39 +0600

    crud

---
+++ src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>
+        Email : kawsarm104@gmail.com
+        facebook:facebook.com/kawsarm104
+        linkedin:linkedin.com/kawsarm104
+        </h1>
+       
+        </div>
+    </div>
+  );
+};
+
+export default About;
